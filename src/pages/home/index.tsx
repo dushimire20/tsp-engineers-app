15951eda0372ae75ef35407c5420476ded4803ce
dushimire20 <odushimire922@daviscollege.com>
2024-11-13 19:36:30 +0200

    third commit

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HomeImage from "@/assets/Home-1.png";
-import Desktop from "@/assets/Desktop.png"
+import Desktop from "@/assets/Desktop.png";
+import OurProject from "@/assets/OurProject.png"
 
 type Props = {};
 
@@ -21,7 +22,7 @@ const Home = (props: Props) => {
         {/* new section */}
 
         <div className="md:flex w-full gap-8 mt-4">
-          <div className=" flex-col w-full md:w-[286px] rounded-sm">
+          <div className=" flex-col w-full md:w-[286px] rounded-2xl shadow-sm border-2 px-2 py-3">
             <img alt="" className="" src={Desktop} />
             <h1
             className="font-inter font-medium text-[21px] leading-[26.7px] tracking-[-0.13px]"
@@ -32,7 +33,7 @@ const Home = (props: Props) => {
 
           </div>
 
-          <div className=" flex-col w-full md:w-[286px] rounded-sm">
+          <div className=" flex-col w-full md:w-[286px] rounded-2xl shadow-sm border-2 px-2 py-3">
             <img alt="" className="" src={Desktop} />
             <h1
             className="font-inter font-medium md:text-[21px] md:leading-[26.7px] tracking-[-0.13px]"
@@ -43,7 +44,7 @@ const Home = (props: Props) => {
 
           </div>
 
-          <div className=" flex-col w-full md:w-[286px] rounded-sm">
+          <div className=" flex-col w-full md:w-[286px] rounded-2xl shadow-sm border-2 px-2 py-3">
             <img alt="" className="" src={Desktop} />
             <h1
             className="font-inter font-medium md:text-[21px] leading-[26.7px] tracking-[-0.13px]"
@@ -54,7 +55,7 @@ const Home = (props: Props) => {
 
           </div>
 
-          <div className=" flex-col w-full md:w-[286px] rounded-sm">
+          <div className=" flex-col w-full md:w-[286px] rounded-2xl shadow-sm border-2 px-2 py-3">
             <img alt="" className="" src={Desktop} />
             <h1
             className="font-inter font-medium md:text-[21px] md:leading-[26.7px] tracking-[-0.13px]"
@@ -65,6 +66,168 @@ const Home = (props: Props) => {
 
           </div>
         </div>
+
+        {/* new section */}
+
+        <div className="md:flex w-full gap-8 mt-4">
+          <div className=" flex-col w-full md:w-[286px] rounded-2xl shadow-sm border-2 px-2 py-3">
+            <img alt="" className="" src={Desktop} />
+            <h1
+            className="font-inter font-medium text-[21px] leading-[26.7px] tracking-[-0.13px]"
+            >Water Pump</h1>
+            <p
+            className="font-inter font-normal text-[16.12px] leading-[24.19px] tracking-[-0.13px]"
+            >This template contains product showcase of the business, suitable for business with large number of products or services.</p>
+
+          </div>
+
+          <div className=" flex-col w-full md:w-[286px] rounded-2xl shadow-sm border-2 px-2 py-3">
+            <img alt="" className="" src={Desktop} />
+            <h1
+            className="font-inter font-medium md:text-[21px] md:leading-[26.7px] tracking-[-0.13px]"
+            >Water Pump</h1>
+            <p
+            className="font-inter font-normal md:text-[16.12px] md:leading-[24.19px] tracking-[-0.13px]"
+            >This template contains product showcase of the business, suitable for business with large number of products or services.</p>
+
+          </div>
+
+          <div className=" flex-col w-full md:w-[286px] rounded-2xl shadow-sm border-2 px-2 py-3">
+            <img alt="" className="" src={Desktop} />
+            <h1
+            className="font-inter font-medium md:text-[21px] leading-[26.7px] tracking-[-0.13px]"
+            >Water Pump</h1>
+            <p
+            className="font-inter font-normal md:text-[16.12px] md:leading-[24.19px] tracking-[-0.13px]"
+            >This template contains product showcase of the business, suitable for business with large number of products or services.</p>
+
+          </div>
+
+          <div className=" flex-col w-full md:w-[286px] rounded-2xl shadow-sm border-2 px-2 py-3">
+            <img alt="" className="" src={Desktop} />
+            <h1
+            className="font-inter font-medium md:text-[21px] md:leading-[26.7px] tracking-[-0.13px]"
+            >Water Pump</h1>
+            <p
+            className="font-inter font-normal text-[16.12px] leading-[24.19px] tracking-[-0.13px]"
+            >This template contains product showcase of the business, suitable for business with large number of products or services.</p>
+
+          </div>
+        </div>
+
+        {/* sect !! */}
+
+        <div className="mx-auto text-center w-[436px]">
+          <p className="text-center font-inter mt-6 md:text-[20px] tracking-tight font-normal leading-7">
+            Our projects
+          </p>
+          <p className="text-center font-sa mt-2 md:text-[48px] tracking-tight font-medium leading-[62.4px]">
+            Style and Sensation <br/> Memory and Magic
+          </p>
+        </div>
+
+        <div className="md:flex w-full gap-8 mt-4">
+          <div className=" flex-col w-full md:w-[429.33x] rounded-2xl shadow-sm border-2 px-2 py-3">
+            <img alt="" className="" src={OurProject} />
+            <h1
+            className="font-inter font-medium text-[21px] leading-[26.7px] tracking-[-0.13px]"
+            >Water Pump</h1>
+            <p
+            className="font-inter font-normal text-[16.12px] leading-[24.19px] tracking-[-0.13px]"
+            >This template contains product showcase of the business, suitable for business with large number of products or services.</p>
+            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+            </a>
+          </div>
+
+          <div className=" flex-col w-full md:w-[429.33x] rounded-2xl shadow-sm border-2 px-2 py-3">
+            <img alt="" className="" src={OurProject} />
+            <h1
+            className="font-inter font-medium text-[21px] leading-[26.7px] tracking-[-0.13px]"
+            >Water Pump</h1>
+            <p
+            className="font-inter font-normal text-[16.12px] leading-[24.19px] tracking-[-0.13px]"
+            >This template contains product showcase of the business, suitable for business with large number of products or services.</p>
+            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+            </a>
+          </div>
+
+          <div className=" flex-col w-full md:w-[429.33x] rounded-2xl shadow-sm border-2 px-2 py-3">
+            <img alt="" className="" src={OurProject} />
+            <h1
+            className="font-inter font-medium text-[21px] leading-[26.7px] tracking-[-0.13px]"
+            >Water Pump</h1>
+            <p
+            className="font-inter font-normal text-[16.12px] leading-[24.19px] tracking-[-0.13px]"
+            >This template contains product showcase of the business, suitable for business with large number of products or services.</p>
+            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+            </a>
+          </div>
+        </div>
+
+        <div className="md:flex w-full gap-8 mt-4">
+          <div className=" flex-col w-full md:w-[429.33x] rounded-2xl shadow-sm border-2 px-2 py-3">
+            <img alt="" className="" src={OurProject} />
+            <h1
+            className="font-inter font-medium text-[21px] leading-[26.7px] tracking-[-0.13px]"
+            >Water Pump</h1>
+            <p
+            className="font-inter font-normal text-[16.12px] leading-[24.19px] tracking-[-0.13px]"
+            >This template contains product showcase of the business, suitable for business with large number of products or services.</p>
+            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+            </a>
+          </div>
+
+          <div className=" flex-col w-full md:w-[429.33x] rounded-2xl shadow-sm border-2 px-2 py-3">
+            <img alt="" className="" src={OurProject} />
+            <h1
+            className="font-inter font-medium text-[21px] leading-[26.7px] tracking-[-0.13px]"
+            >Water Pump</h1>
+            <p
+            className="font-inter font-normal text-[16.12px] leading-[24.19px] tracking-[-0.13px]"
+            >This template contains product showcase of the business, suitable for business with large number of products or services.</p>
+            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+            </a>
+          </div>
+
+          <div className=" flex-col w-full md:w-[429.33x] rounded-2xl shadow-sm border-2 px-2 py-3">
+            <img alt="" className="" src={OurProject} />
+            <h1
+            className="font-inter font-medium text-[21px] leading-[26.7px] tracking-[-0.13px]"
+            >Water Pump</h1>
+            <p
+            className="font-inter font-normal text-[16.12px] leading-[24.19px] tracking-[-0.13px]"
+            >This template contains product showcase of the business, suitable for business with large number of products or services.</p>
+            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+            </a>
+          </div>
+        </div>
+
+
+
       </div>
     </section>
   );
