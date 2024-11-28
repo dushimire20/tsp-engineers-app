f952e6023fb15b97886c4a931771125afa236761
dushimire20 <odushimire922@daviscollege.com>
2024-11-28 11:02:41 +0200

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import Home from '@/pages/home';
 import Layout from '@/components/Layout';
 import AboutUs from '@/pages/about';
 import ContactUs from '@/pages/contact';
-import ServiceList from '@/components/ServiceList'; // Updated component
-import SubServiceList from "@/components/subServiceList"; // Updated component
-import ProductList from '@/components/ProductList'; // Updated component
-import ProductDetails from "@/components/ProductDeatails"; // Updated component
-import sampleData from "@/data/services"; // Import the sample data
+
 
 const App: React.FC = () => {
   return (
@@ -18,21 +14,6 @@ const App: React.FC = () => {
           <Route index element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/contact" element={<ContactUs />} />
-
-          {/* Service Routes */}
-          <Route path="/services" element={<ServiceList services={sampleData} />} />
-          <Route
-            path="/services/:serviceId"
-            element={<SubServiceList services={sampleData} />}
-          />
-          <Route
-            path="/services/:serviceId/:subServiceId"
-            element={<ProductList services={sampleData} />}
-          />
-          <Route
-            path="/services/:serviceId/:subServiceId/:productId"
-            element={<ProductDetails services={sampleData} />}
-          />
         </Route>
       </Routes>
     </Router>
